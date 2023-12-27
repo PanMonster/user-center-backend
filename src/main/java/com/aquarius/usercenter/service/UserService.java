36c64ece353e2ec273722c656832f62d1faa2c90
aquarius <1102260487@qq.com>
2023-12-27 10:10:36 +0800

    feat: 添加用户登录功能

--- src/main/java/com/aquarius/usercenter/service/UserService.java
+++ src/main/java/com/aquarius/usercenter/service/UserService.java
@@ -2,6 +2,7 @@ package com.aquarius.usercenter.service;
 
 import com.aquarius.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * 用户服务
@@ -18,5 +19,15 @@ public interface UserService extends IService<User> {
      * @return 新户用 id
      */
     Long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账号
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    User doLogin(String userAccount, String userPassword, HttpServletRequest request);
 
 }
--- src/main/java/com/aquarius/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/aquarius/usercenter/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.aquarius.usercenter.service.UserService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
@@ -19,16 +21,28 @@ import java.util.regex.Pattern;
  * @createDate 2023-12-26 01:39:35
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         implements UserService {
 
     @Resource
     private UserMapper userMapper;
 
+    /**
+     * 盐（用于混交加密）
+     */
+    public static final String SALT = "goblin";
+
+    /**
+     * 用户登录态键
+     */
+    public static final String USER_LOGIN_STATE = "userLoginState";
+
     @Override
     public Long userRegister(String userAccount, String userPassword, String checkPassword) {
         //1. 校验
         if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            // todo 修改为自定义异常
             return -1L;
         }
         if (userAccount.length() < 4) {
@@ -55,7 +69,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
             return -1L;
         }
         //2. 加密
-        final String SALT = "goblin";
         String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
         //3. 保存
         User user = new User();
@@ -66,5 +79,51 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
             return -1L;
         }
         return user.getId();
+    }
+
+    @Override
+    public User doLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        //1. 校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
+            return null;
+        }
+        if (userAccount.length() < 4) {
+            return null;
+        }
+        if (userPassword.length() < 8) {
+            return null;
+        }
+        // 账户不含特殊字符
+        String validPattern = "[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        if (matcher.find()) {
+            return null;
+        }
+        //2. 加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        // 查询用户是否存在
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_account", userAccount);
+        queryWrapper.eq("user_password", encryptPassword);
+        User user = userMapper.selectOne(queryWrapper);
+        // 用户不存在
+        if (user == null) {
+            log.info("user login failed, userAccount cannot match userPassword");
+            return null;
+        }
+        // 3. 用户脱敏
+        User safetyUser = new User();
+        safetyUser.setId(user.getId());
+        safetyUser.setUsername(user.getUsername());
+        safetyUser.setUserAccount(user.getUserAccount());
+        safetyUser.setAvatarUrl(user.getAvatarUrl());
+        safetyUser.setGender(user.getGender());
+        safetyUser.setPhone(user.getPhone());
+        safetyUser.setEmail(user.getEmail());
+        safetyUser.setUserStatus(user.getUserStatus());
+        safetyUser.setCreateTime(user.getCreateTime());
+        // 4. 记录用户的登录态
+        request.getSession().setAttribute(USER_LOGIN_STATE, safetyUser);
+        return safetyUser;
     }
 }
