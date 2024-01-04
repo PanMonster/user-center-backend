89c36990731baa52ee4540f56357d55f284d4b52
aquarius <1102260487@qq.com>
2024-01-04 16:50:05 +0800

    add: 生产环境配置

---
+++ sql/create_table.sql
@@ -0,0 +1,20 @@
+-- auto-generated definition
+create table user
+(
+    id            bigint auto_increment comment 'id'
+        primary key,
+    username      varchar(256)                        null comment '用户名',
+    user_account  varchar(256)                        null comment '登录账号',
+    avatar_url    varchar(1024)                       null comment '用户头像',
+    gender        tinyint                             null comment '性别',
+    user_password varchar(512)                        not null comment '密码',
+    phone         varchar(128)                        null comment '手机号',
+    email         varchar(512)                        null comment '邮箱',
+    user_status   int       default 0                 not null comment '用户状态 0 - 正常',
+    create_time   timestamp default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time   timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete     tinyint   default 0                 not null comment '是否删除',
+    user_role     int       default 0                 not null comment '用户角色 0 - 普通用户 1 - 管理员'
+)
+    comment '用户表';
+
